59573a7b2708dd51822e4f56915dcbac989008c9
avinashchowdary1998 <99256395+avinashchowdary1998@users.noreply.github.com>
2023-05-25 18:57:45 +0530

    Update script.js

--- src/assets/script.js
+++ src/assets/script.js
@@ -129,14 +129,6 @@ function pay(amount) {
     }
     return cashReturned;
 }
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
-
-
-/* The following is for running unit tests. 
-   To fully complete this project, it is expected that all tests pass.
-   Run the following command in terminal to run tests
-   npm run test
-*/
 
 module.exports = {
   products,
